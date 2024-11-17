474936fae591d8b0ea6cb69d956a9cdf1658f370
Kumario1 <pkumar13.ipec@gmail.com>
2024-11-16 18:12:41 -0600

    Updated app.py

--- app.py
+++ app.py
@@ -10,3 +10,26 @@ import matplotlib.pyplot as plt
 from PIL import Image, ImageDraw, ImageFont
 import base64
 from functions import *
+
+st.set_page_config(layout="wide",page_title="Carbon Footprint Calculator", page_icon="./media/favicon.ico")
+
+def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+background = get_base64("./media/background_min.jpg")
+icon2 = get_base64("./media/icon2.png")
+icon3 = get_base64("./media/icon3.png")
+
+with open("./style/style.css", "r") as style:
+    css=f"""<style>{style.read().format(background=background, icon2=icon2, icon3=icon3)}</style>"""
+    st.markdown(css, unsafe_allow_html=True)
+
+def script():
+    with open("./style/scripts.js", "r", encoding="utf-8") as scripts:
+        open_script = f"""<script>{scripts.read()}</script> """
+        html(open_script, width=0, height=0)
+
+left, middle, right = st.columns([2,3.5,2])
+main, comps , result = middle.tabs([" ", " ", " "])
